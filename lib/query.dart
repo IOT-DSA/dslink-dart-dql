b2044722ae0620a54b677cfd875029f22964e0b8
Kenneth Endfinger <kaendfinger@gmail.com>
2016-02-28 16:35:11 -0500

    Extract basic context into it's own file.

--- lib/query.dart
+++ lib/query.dart
@@ -21,6 +21,8 @@ part "src/commands/drop.dart";
 part "src/commands/expression.dart";
 part "src/commands/rename.dart";
 
+part "src/query/basic_context.dart";
+
 final Map<String, QueryProcessorFactory> QUERY_COMMANDS = {
   "list": (QueryContext context) => new ListNodeQueryProcessor(context),
   "subscribe": (QueryContext context) => new SubscribeQueryProcessor(context),
@@ -32,52 +34,3 @@ final Map<String, QueryProcessorFactory> QUERY_COMMANDS = {
   "rename": (QueryContext context) => new RenameQueryProcessor(context),
   "where": (QueryContext context) => new FilterQueryProcessor(context)
 };
-
-class BasicQueryContext extends QueryContext {
-  final Requester requester;
-
-  BasicQueryContext(this.requester);
-
-  @override
-  Stream<QueryUpdate> query(String input) {
-    var processors = parse(input);
-    return processQuery(processors);
-  }
-
-  List<QueryProcessor> parse(String input) {
-    logger.fine("Run Query: ${input}");
-
-    List<QueryStatement> statements = parseQueryInput(input);
-
-    logger.fine("Parse Query: ${statements}");
-
-    List<QueryProcessor> processors = statements.map((QueryStatement statement) {
-      if (!QUERY_COMMANDS.containsKey(statement.command)) {
-        throw new QueryException(
-          "Failed to parse query: unknown command '${statement.command}'"
-        );
-      }
-
-      QueryProcessor processor = QUERY_COMMANDS[statement.command](this);
-      processor.init(statement);
-      return processor;
-    }).toList();
-
-    return processors;
-  }
-
-  @override
-  Stream<RequesterListUpdate> list(String path) {
-    return requester.list(path);
-  }
-
-  @override
-  StreamSubscription subscribe(String path, callback(ValueUpdate update)) {
-    return requester.subscribe(path, callback);
-  }
-
-  @override
-  Future<RemoteNode> getRemoteNode(String path) {
-    return requester.getRemoteNode(path);
-  }
-}
---
+++ lib/src/query/basic_context.dart
@@ -0,0 +1,52 @@
+part of dslink.dql.query;
+
+class BasicQueryContext extends QueryContext {
+  final Requester requester;
+
+  BasicQueryContext(this.requester);
+
+  @override
+  Stream<QueryUpdate> query(String input) {
+    var processors = parse(input);
+    return processQuery(processors);
+  }
+
+  List<QueryProcessor> parse(String input) {
+    logger.fine("Run Query: ${input}");
+
+    List<QueryStatement> statements = parseQueryInput(input);
+
+    logger.fine("Parse Query: ${statements}");
+
+    List<QueryProcessor> processors = statements.map((QueryStatement statement) {
+      if (!QUERY_COMMANDS.containsKey(statement.command)) {
+        throw new QueryException(
+          "Failed to parse query: unknown command '${statement.command}'"
+        );
+      }
+
+      QueryProcessor processor = QUERY_COMMANDS[statement.command](this);
+      processor.init(statement);
+      return processor;
+    }).toList();
+
+    return processors;
+  }
+
+  @override
+  Stream<RequesterListUpdate> list(String path) {
+    return requester.list(path);
+  }
+
+  @override
+  StreamSubscription subscribe(String path, callback(ValueUpdate update), [
+    int qos = 0
+  ]) {
+    return requester.subscribe(path, callback, qos);
+  }
+
+  @override
+  Future<RemoteNode> getRemoteNode(String path) {
+    return requester.getRemoteNode(path);
+  }
+}
