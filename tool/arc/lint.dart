71ff6c51cf1e48bc8c1cbbdc065fd5aa744a79b8
Kenneth Endfinger <kaendfinger@gmail.com>
2016-03-04 22:16:01 -0500

    Improve lint host.

--- tool/arc/lint.dart
+++ tool/arc/lint.dart
@@ -1,4 +1,11 @@
+import "dart:async";
 import "dart:io";
+
+import "package:path/path.dart" as pathlib;
+
+const List<String> ALWAYS_IGNORE_FILES = const [
+  "lib/script.dart"
+];
 
 const Map<String, String> SEVERITY = const {
   "WARNING": "warning",
@@ -14,13 +21,25 @@ main(List<String> files) async {
   var result = await Process.run("dartanalyzer", [
     "--format=machine",
   ]..addAll(files));
-
+  
   List<String> lines = [];
   lines.addAll(result.stdout.toString().split("\n"));
   lines.addAll(result.stderr.toString().split("\n"));
 
+  List<String> figureItOut = [];
+
   for (String line in lines) {
     line = line.trim();
+
+    if (line.contains('is a part and cannot be analyzed')) {
+      var path = line.split(" is a part").first.trim();
+      figureItOut.add(path);
+      continue;
+    }
+
+    if (line == "Please pass in a library that contains this part.") {
+      continue;
+    }
 
     if (line.isEmpty) continue;
 
@@ -37,8 +56,48 @@ main(List<String> files) async {
     int columnNumber = int.parse(parts[5]);
     String message = parts[7];
 
+    path = pathlib.relative(path, from: Directory.current.path);
+    if (ALWAYS_IGNORE_FILES.contains(path)) {
+      continue;
+    }
+
     print("${severity} !!${path}!!"
       " !!${name}!! ${lineNumber}:${columnNumber}"
       " !!${message}!!");
+  }
+
+  for (String path in figureItOut) {
+    await lookForRealFileAndAnalyze(path);
+  }
+}
+
+Future lookForRealFileAndAnalyze(String path) async {
+  var file = new File(path);
+  var content = await file.readAsString();
+  var firstLine = content.trim().split("\n").first;
+  if (!firstLine.startsWith("part of ")) {
+    return;
+  }
+
+  var lib = firstLine.split("part of ").last.trim();
+  if (lib.endsWith(";")) {
+    lib = lib.substring(0, lib.length - 1);
+  }
+
+  var dir = new Directory("lib");
+  var files = await dir.list(recursive: true).where((entity) {
+    return entity is File && entity.path.endsWith(".dart");
+  }).toList();
+
+  for (File file in files) {
+    var lines = await file.readAsLines();
+    var line = lines.where((line) => line.trim().isNotEmpty).first;
+
+    if (line.startsWith("library ${lib};")) {
+      await main([
+        file.path
+      ]);
+      return;
+    }
   }
 }
