5d48dc4a645aa38f81027755e929d76e345cf60d
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-04-27 22:13:44 -0400

    Fix Upload Script Again

--- tool/upload.sh
+++ tool/upload.sh
@@ -2,7 +2,7 @@
 set -e
 TMP=$(mktemp -d)
 MD=$(realpath "$(dirname $0)/..")
-git clone https://github.com/IOT-DSA/dslink-dart-dql.git -b gh-pages "${TMP}"
+git clone git@github.com:IOT-DSA/dslink-dart-dql.git -b gh-pages "${TMP}"
 pub build example/browser
 rm -rf ${TMP:?}/*
 cp -R build/example/browser/*  "${TMP:?}/"
